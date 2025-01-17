f14359e416cc88c0aea3a841fb7845a7b0959478
Cristian Sotomayor <csotomayorcompsci@gmail.com>
2025-01-16 23:49:59 -0800

    refactored initializer parameters and separated image loading func

--- src/hooks/useWebGL.ts
+++ src/hooks/useWebGL.ts
@@ -1,30 +1,33 @@
 import { useEffect, useRef, useState } from "react";
-import { Renderer } from "../utils/Renderer";
+import { Renderer } from "../renderers/Renderer";
+import { mapFrag, mapVert, colorVert, colorFrag, lineVert, lineFrag } from "../shaders";
 
 export const useWebGL = (canvasRef: React.RefObject<HTMLCanvasElement>) => {
   const rendererRef = useRef<Renderer | null>(null);
-  const [didLoad, setLoad] = useState<boolean>(false);
-  const [initializationError, setInitializationError] = useState<string | null>(
-    null
-  );
+  const [didLoad, setLoad] = useState(false);
+  const [initializationError, setInitializationError] = useState<string | null>(null);
 
   useEffect(() => {
     const initRenderer = async () => {
       if (!canvasRef.current) return;
       rendererRef.current = new Renderer(canvasRef.current);
-
       try {
-        await rendererRef.current.initialize();
+        await rendererRef.current.initialize(
+          mapVert,
+          mapFrag,
+          colorVert,
+          colorFrag,
+          lineVert,
+          lineFrag,
+          "/map.jpg",
+          "/plane2.png"
+        );
         setLoad(!didLoad);
       } catch (error) {
-        setInitializationError(
-          error instanceof Error ? error.message : String(error)
-        );
+        setInitializationError(error instanceof Error ? error.message : String(error));
       }
     };
-
     initRenderer();
-
     return () => {
       if (rendererRef.current) rendererRef.current.dispose();
     };
