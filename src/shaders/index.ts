eb58e5e103f4c5387c54eb0694a39ed5e553e6fb
Cristian Sotomayor <csotomayorcompsci@gmail.com>
2025-01-16 23:49:12 -0800

    fixed shader imports

---
+++ src/shaders/index.ts
@@ -0,0 +1,8 @@
+import mapVert from "./map/map.vert.glsl?raw";
+import mapFrag from "./map/map.frag.glsl?raw";
+import colorVert from "./color/color.vert.glsl?raw";
+import colorFrag from "./color/color.frag.glsl?raw";
+import lineVert from "./line/line.vert.glsl?raw";
+import lineFrag from "./line/line.frag.glsl?raw";
+
+export { mapFrag, mapVert, colorVert, colorFrag, lineVert, lineFrag };
